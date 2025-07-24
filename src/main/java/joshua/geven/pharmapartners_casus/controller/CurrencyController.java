2183c07da61899dced8befa58ace1a69905374a6
JGeven <joshua.geven@hotmail.com>
2025-07-25 00:24:50 +0200

    Added CORS

--- src/main/java/joshua/geven/pharmapartners_casus/controller/CurrencyController.java
+++ src/main/java/joshua/geven/pharmapartners_casus/controller/CurrencyController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/currencies")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CurrencyController {
 
     private final CurrencyService currencyService;
